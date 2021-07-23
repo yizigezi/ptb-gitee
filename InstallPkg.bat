ac45751c876710eb8514ab9127d52a8be05565b8
yizigezi <jjy1207826398@163.com>
2021-07-23 13:36:57 +0000

    增加依赖包安装脚本

---
+++ InstallPkg.bat
@@ -0,0 +1,6 @@
+python.exe -m pip install pip install requests
+python.exe -m pip install pip install pygame
+python.exe -m pip install pip install pillow
+python.exe -m pip install pymediainfo
+
+pause
