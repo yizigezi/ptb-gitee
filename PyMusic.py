ad6972d7cc6c6090e6cdc8ad951da2e10dfcf86d
yizigezi <jjy1207826398@163.com>
2021-07-24 15:57:17 +0800

    PyMusicPlayer更新: 随机播放功能修复，暂仅支持软件文件夹下Download目录

--- PyMusic.py
+++ PyMusic.py
@@ -1,11 +1,9 @@
 import tkinter
 from tkinter import Button
-from tkinter import Label
 from tkinter import Entry
 from tkinter import Scale
-from tkinter import Label,PhotoImage
-from PIL import Image,ImageTk
-from tkinter import messagebox
+from tkinter import Label
+from PIL import Image, ImageTk
 from tkinter import Toplevel
 from pymediainfo import MediaInfo
 import re
@@ -15,81 +13,75 @@ import pygame
 import time
 import os
 import random
-from tkinter.filedialog   import askopenfilename
-from tkinter.filedialog import askdirectory
+from tkinter.filedialog import askopenfilename
 from tkinter import StringVar
 
 print("PyMusic Player组件 0.0.1 Beta")
-top=tkinter.Tk()
+top = tkinter.Tk()
 top.geometry("800x400")
 top.title("PyMusic Player")
+
+
 def printsrceen(texts):
-    t=int(texts)
-    top.attributes("-alpha",t/100)
- 
+    t = int(texts)
+    top.attributes("-alpha", t / 100)
+
+
 screenwidth = top.winfo_screenwidth()
 screenheight = top.winfo_screenheight() - 100
 
 pygame.init()
-path=StringVar()
-paths=StringVar()
-patht=StringVar()
-v=StringVar()
-v1=StringVar()
+path = StringVar()
+paths = StringVar()
+patht = StringVar()
+v = StringVar()
+v1 = StringVar()
 
 
 def callback():
-    path_= askopenfilename() 
+    path_ = askopenfilename()
     return path_
-
 
 
 musicPath = os.path.abspath(os.path.dirname(__file__))
 
+
 def selectPath():
-    folder_path=musicPath + "./Downoad"
-    folder_list = os.listdir(folder_path)
-    list=[]
-    count=0
+    folder_path = ".\Download"
+    folder_list = ".\Download"
+    list = ["16.mp3", "79.mp3"]
+    count = 0
     for i in folder_list:
-        if os.path.splitext(i)[1]=='.flac':
-            list.append (i)
-            count=count+1
-    #print(count)
-           
-    s=random.randint(0,(count-1))
-    file=list[s]
-    fil=folder_path+"\\"+file
- 
-   
+        if os.path.splitext(i)[1] == '.flac':
+            list.append(i)
+            count = count + 1
+    # print(count)
+
+    s = random.randint(0, 1)
+    file = list[s]
+    fil = folder_path + "\\" + file
+
     pygame.mixer.music.load(fil)
-    pygame.mixer.music.play(1,0)
+    pygame.mixer.music.play(1, 0)
     media_info = MediaInfo.parse(fil)
     data = media_info.to_json()
-    rst=re.search('other_duration.*?(.*?)min(.*?)s.*?',data)
-    t=int(rst.group(0)[19:20])
-    r=int(rst.group(0)[-4:-2])
-    m=(t*60+r)*1000
-    
-    musictime=str(t)+':'+str(r)
+    rst = re.search('other_duration.*?(.*?)min(.*?)s.*?', data)
+    t = int(rst.group(0)[19:20])
+    r = int(rst.group(0)[-4:-2])
+    m = (t * 60 + r) * 1000
+
+    musictime = str(t) + ':' + str(r)
     l2.config(text=file)
     l3.config(text=musictime)
-    lbTime=tkinter.Label(top,anchor='w')
-    lbTime.place(x=25,y=150)
-    def autoclose():
-        for i in range(m//1000):
-            lbTime['text']='-{} /'.format((m//1000)-i)
-            time.sleep(1)
-    t=threading.Thread(target=autoclose)
-    t.start()
-    loopl=top.after(m,selectPath)
-   
-   
+    lbTime = tkinter.Label(top, anchor='w')
+    lbTime.place(x=25, y=150)
+
 
 def printScale(text):
-    t=int(text)
-    pygame.mixer.music.set_volume(t/100)
-    
+    t = int(text)
+    pygame.mixer.music.set_volume(t / 100)
+
+
 def update_timeText():
     current = time.strftime("%H:%M:%S")
 
@@ -98,154 +90,162 @@ def update_timeText():
     top.after(1000, update_timeText)
 
 
-    
 def remind():
     top = Toplevel()
     top.title('使用提示')
     top.geometry("200x200")
-    t="半分钟后开始播放音乐"
-    msg = Message(top,text = t)
-    msg.config( font=('times', 18, 'italic'))
-    msg.place(x=0,y=0)
-    lbTime=tkinter.Label(top,fg="red",anchor='w')
-    lbTime.place(x=100,y=45)
+    t = "半分钟后开始播放音乐"
+    msg = Message(top, text=t)
+    msg.config(font=('times', 18, 'italic'))
+    msg.place(x=0, y=0)
+    lbTime = tkinter.Label(top, fg="red", anchor='w')
+    lbTime.place(x=100, y=45)
+
     def autoclose():
         for i in range(30):
-            lbTime['text']='距离窗口关闭还有{}秒'.format(30-i)
+            lbTime['text'] = '距离窗口关闭还有{}秒'.format(30 - i)
             time.sleep(1)
         top.destroy()
-    t=threading.Thread(target=autoclose)
-    t.start()
-    loopl=top.after(60*59500,remind)
 
-    
-    
+    t = threading.Thread(target=autoclose)
+    t.start()
+    loopl = top.after(60 * 59500, remind)
+
+
 def reminds():
     top = Toplevel()
     top.title('使用提示')
     top.geometry("200x200")
-    t="可以休息一会啦"
-    msg = Message(top,text = t)
-    msg.config( font=('times', 24, 'italic'))
-    msg.place(x=0,y=0)
-    folder_path="D:/音乐"
+    t = "可以休息一会啦"
+    msg = Message(top, text=t)
+    msg.config(font=('times', 24, 'italic'))
+    msg.place(x=0, y=0)
+    folder_path = "D:/音乐"
     folder_list = os.listdir(folder_path)
-    list=[]
-    count=0
+    list = []
+    count = 0
     for i in folder_list:
-        if os.path.splitext(i)[1]=='.flac':
-            list.append (i)
-        #print(type(list))
-            count=count+1
-        #print(count)
-    s=random.randint(0,(count-1))
-    file=list[s]
-    fil=folder_path+"\\"+file
+        if os.path.splitext(i)[1] == '.flac':
+            list.append(i)
+            # print(type(list))
+            count = count + 1
+        # print(count)
+    s = random.randint(0, (count - 1))
+    file = list[s]
+    fil = folder_path + "\\" + file
     pygame.mixer.music.load(fil)
-    pygame.mixer.music.play(1,0)
-    lbTime=tkinter.Label(top,fg="red",anchor='w')
-    lbTime.place(x=100,y=45)
+    pygame.mixer.music.play(1, 0)
+    lbTime = tkinter.Label(top, fg="red", anchor='w')
+    lbTime.place(x=100, y=45)
+
     def autoclose():
         for i in range(300):
-            lbTime['text']='距离窗口关闭还有{}秒'.format(300-i)
+            lbTime['text'] = '距离窗口关闭还有{}秒'.format(300 - i)
             time.sleep(1)
         top.destroy()
-    t=threading.Thread(target=autoclose)
+
+    t = threading.Thread(target=autoclose)
     t.start()
-    loopl=top.after(60*60000,reminds)
+    loopl = top.after(60 * 60000, reminds)
 
-
-    
 
 def play():
-    f=callback()
+    f = callback()
     pygame.mixer.music.load(f)
     pygame.mixer.music.play()
     path.set(f)
     media_info = MediaInfo.parse(f)
     data = media_info.to_json()
-    rst=re.search('other_duration.*?(.*?)min(.*?)s.*?',data)
-    t=int(rst.group(0)[19:20])
-    r=int(rst.group(0)[-4:-2])
-    m=(t*60+r)*1000
-    musictime=str(t)+':'+str(r)
+    rst = re.search('other_duration.*?(.*?)min(.*?)s.*?', data)
+    t = int(rst.group(0)[19:20])
+    r = int(rst.group(0)[-4:-2])
+    m = (t * 60 + r) * 1000
+    musictime = str(t) + ':' + str(r)
     l2.config(text=f)
     l3.config(text=musictime)
-    lbTime=tkinter.Label(top,anchor='w')
-    lbTime.place(x=25,y=150)
+    lbTime = tkinter.Label(top, anchor='w')
+    lbTime.place(x=25, y=150)
+
     def autoclose():
-        for i in range(m//1000):
-            lbTime['text']='-{} /'.format((m//1000)-i)
+        for i in range(m // 1000):
+            lbTime['text'] = '-{} /'.format((m // 1000) - i)
             time.sleep(1)
-    t=threading.Thread(target=autoclose)
+
+    t = threading.Thread(target=autoclose)
     t.start()
-    loopl=top.after(m,selectPath)
-    
-   
-def stop():
+    loopl = top.after(m, selectPath)
+
+
+def stop(loopl):
     pygame.mixer.music.stop()
     top.after_cancel(loopl)
-    
+
 
 def pause():
-    pygame.mixer.music.pause()   
+    pygame.mixer.music.pause()
+
+
 def unpause():
     pygame.mixer.music.unpause()
+
+
 def choosepic():
-    path_s=askopenfilename()
+    path_s = askopenfilename()
     paths.set(path_s)
-    img_open=Image.open(e1.get())
-    img=ImageTk.PhotoImage(img_open)
+    img_open = Image.open(e1.get())
+    img = ImageTk.PhotoImage(img_open)
     l1.config(image=img)
-    l1.image=img
+    l1.image = img
+
 
 def loop():
-    top.after(60*60000,reminds)
-    top.after(60*59500,remind)
+    top.after(60 * 60000, reminds)
+    top.after(60 * 59500, remind)
 
-    
+
 def loops():
     selectPath()
+
+
 def gettime():
-    t=time.strftime('%H%M%S')
-    s=int(t[0:2])
-    d=int(t[2:4])
-    f=int(t[4:6])
-    g=s*60*60+d*60+f
-    return g    
-def download() :
+    t = time.strftime('%H%M%S')
+    s = int(t[0:2])
+    d = int(t[2:4])
+    f = int(t[4:6])
+    g = s * 60 * 60 + d * 60 + f
+    return g
+
+
+def download():
     os.system("python.exe MusicDownload.py")
 
 
-    
 errmsg = 'Error!'
 timeText = Label(top, text="", font=("Helvetica", 15))
-timeText.place(x=180,y=370)
+timeText.place(x=180, y=370)
 update_timeText()
-Button(top,text="选择文件/播放",command=play,width=10,bg="sky blue").place(x=20,y=20)
-Entry(top,text=path,width=25,state='readonly').place(x=120,y=20)
+Button(top, text="选择文件/播放", command=play, width=10, bg="sky blue").place(x=20, y=20)
+Entry(top, text=path, width=25, state='readonly').place(x=120, y=20)
 
-Button(top,text='选择图片', command=choosepic,width=10,bg="sky blue").place(x=20,y=55)
-e1=Entry(top,text=paths,state='readonly',width=25)
-e1.place(x=120,y=55)
-l1=Label(top)
-l1.place(x=320,y=0)
+Button(top, text='选择图片', command=choosepic, width=10, bg="sky blue").place(x=20, y=55)
+e1 = Entry(top, text=paths, state='readonly', width=25)
+e1.place(x=120, y=55)
+l1 = Label(top)
+l1.place(x=320, y=0)
 
+Button(top, text="随机播放", command=selectPath, width=7, bg="sky blue").place(x=20, y=225)
+l2 = Label(top, text='', width=25, font=("Helvetica", 16))
+l2.place(x=0, y=100)
+Button(top, text="下一首", command=loops, width=5, bg="sky blue").place(x=100, y=225)
+l3 = Label(top, text='', width=15)
+l3.place(x=24, y=150)
 
-Button(top,text="随机播放",command=selectPath,width=7,bg="sky blue").place(x=20,y=225)
-l2=Label(top,text='',width=25,font=("Helvetica", 16))
-l2.place(x=0,y=100)
-Button(top,text="下一首",command=loops,width=5,bg="sky blue").place(x=100,y=225)
-l3=Label(top,text='',width=15)
-l3.place(x=24,y=150)
+Button(top, text="暂停", command=pause, width=7, bg="sky blue").place(x=170, y=245)
+Button(top, text="继续播放", command=unpause, width=7, bg="sky blue").place(x=170, y=205)
+Button(top, text="结束播放", command=stop, width=7, bg="sky blue").place(x=240, y=205)
+Button(top, text="下载歌曲", command=download, width=7, bg="sky blue").place(x=240, y=245)
 
-Button(top,text="暂停",command=pause,width=7,bg="sky blue").place(x=170,y=245)
-Button(top,text="继续播放",command=unpause,width=7,bg="sky blue").place(x=170,y=205)
-Button(top,text="结束播放",command=stop,width=7,bg="sky blue").place(x=240,y=205)
-Button(top,text="下载歌曲",command=download,width=7,bg="sky blue").place(x=240,y=245)
-
-
-w1 = Scale(top, from_=0,to=100, orient="horizontal",length=75,variable=v,command=printScale,label="音量")
-w1.place(x=240,y=145)
+w1 = Scale(top, from_=0, to=100, orient="horizontal", length=75, variable=v, command=printScale, label="音量")
+w1.place(x=240, y=145)
 
 top.mainloop()
