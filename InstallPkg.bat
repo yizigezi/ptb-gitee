b3a7bb35ca3882ad467b0432fff2b6aeb49c6b81
yizigezi <jjy1207826398@163.com>
2021-07-26 17:30:34 +0800

    开发中文件，供参考

--- InstallPkg.bat
+++ InstallPkg.bat
@@ -1,7 +1,13 @@
 python.exe -m pip install pip install requests
+
 python.exe -m pip install pip install pygame
+
 python.exe -m pip install pip install pillow
+
 python.exe -m pip install pymediainfo
+
 python.exe -m pip install Crypto
+
+python.exe -m pip install cos-python-sdk-v5
 
 pause
--- PPkg.py
+++ PPkg.py
@@ -1,10 +1,28 @@
 import requests
 import os
-with open("pkglist.txt", "r") as f:  # 打开文件
-    url = f.read()  # 读取文件
+import sys
+from qcloud_cos import CosConfig
+from qcloud_cos import CosS3Client
+from qcloud_cos.demo import client
 
-headers = {'User-Agent':'OW64; rv:59.0) Chrome/91.0.4472.124'}
-myFile = requests.get(url, headers=headers)
+print("PPkg 包管理器 0.0.1 Beta")
+# appid 已在配置中移除,请在参数 Bucket 中带上 appid。Bucket 由 BucketName-APPID 组成
+# 1. 设置用户配置, 包括 secretId，secretKey 以及 Region
 
-open("./update.exe" , 'wb').write(myFile.content)
-os.system("update.exe")
+secret_id = 'AKIDjHYYHkwrVt0rQjlkSt3vWquhCQN3SZPy'      # 替换为用户的 secretId
+secret_key = 'LPCjzxe3C4fz2nHUQ5TaQzGGzRcW9WbP'      # 替换为用户的 secretKey
+region = 'ap-nanjing'     # 替换为用户的 Region
+
+token = None                # 使用临时密钥需要传入 Token，默认为空，可不填
+response = client.get_presigned_download_url(
+    Bucket='pydos-1301360149',
+    Key='exampleobject',
+    Expired=300,
+    Headers={
+        'Range': 'string'
+    },
+    Params={
+        'param1': 'string',
+        'param2': 'string'
+    }
+)
