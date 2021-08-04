68e8bdf5c67b34bfa3973e86b89272be315ed992
15904323117 <2075929352@qq.com>
2021-08-04 20:39:30 +0800

    compatible with linux/mac

--- PyShell.py
+++ PyShell.py
@@ -5,15 +5,15 @@ tsf = "C:\>"
 zt = "load"
 
 if zt == "load":
-    os.system("python.exe BootLoader.py")
+    os.system("python BootLoader.py")
     zt = "Running"
 
 command = input(tsf)
 while zt == "Running":
-    cCode = "python.exe " + command + ".py"
+    cCode = "python " + command + ".py"
     if command == "exit":
         break
     else:
         os.system(cCode)
-        cCode = "python.exe " + command + ".py"
+        cCode = "python " + command + ".py"
         command = input(tsf)
