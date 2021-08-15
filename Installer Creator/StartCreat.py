66dafe208e88e107785d399bc708a19e645ce526
yizigezi <54109315+yizigezi@users.noreply.github.com>
2021-08-15 22:11:17 +0800

    上传Installer Creator工具

--- Installer Creator/StartCreat.py
+++ Installer Creator/StartCreat.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 print("开始部署")
 url = "https://pydos-1301360149.cos.ap-nanjing.myqcloud.com/installer-creator/config.py"
@@ -7,15 +8,18 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
 }
 filename = "config.py"
-for i in range(2):
+for i in range(2):          # 下载模板文件
     with open(filename, "wb") as f:
         myfile = requests.get(url, headers=headers)
         f.write(myfile.content)
     url = "https://pydos-1301360149.cos.ap-nanjing.myqcloud.com/installer-creator/installer.py"
     filename = "installer.py"
 
+# 下载编译文件
 urlb = "https://pydos-1301360149.cos.ap-nanjing.myqcloud.com/installer-creator/build.bat"
 with open("build.bat", "wb") as b:
     build = requests.get(urlb, headers=headers)
     b.write(build.content)
+r = "https://pypi.tuna.tsinghua.edu.cn/simple"
+os.system("pip install pyside6 -i " + r)
 print("部署完成，请修改config.py以自定义您的安装程序")
