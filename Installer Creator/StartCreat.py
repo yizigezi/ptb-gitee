0b0687dc1b726144b070e3a663eb88b6368286e7
yizigezi <54109315+yizigezi@users.noreply.github.com>
2021-08-15 22:08:18 +0800

    上传Installer Creator工具

--- Installer Creator/StartCreat.py
+++ Installer Creator/StartCreat.py
@@ -1,5 +1,6 @@
 import requests
 
+print("开始部署")
 url = "https://pydos-1301360149.cos.ap-nanjing.myqcloud.com/installer-creator/config.py"
 
 headers = {
@@ -8,7 +9,13 @@ headers = {
 filename = "config.py"
 for i in range(2):
     with open(filename, "wb") as f:
-        myfile = requests.get(url, headers)
+        myfile = requests.get(url, headers=headers)
         f.write(myfile.content)
     url = "https://pydos-1301360149.cos.ap-nanjing.myqcloud.com/installer-creator/installer.py"
     filename = "installer.py"
+
+urlb = "https://pydos-1301360149.cos.ap-nanjing.myqcloud.com/installer-creator/build.bat"
+with open("build.bat", "wb") as b:
+    build = requests.get(urlb, headers=headers)
+    b.write(build.content)
+print("部署完成，请修改config.py以自定义您的安装程序")
